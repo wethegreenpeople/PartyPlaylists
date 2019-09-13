e8f4498ffaf975414fedadf264e860e9bf3e4555
John <wethegreenpeople@gmail.com>
2019-09-13 16:46:15 -0700

    Init commit

---
+++ PartyPlaylists/PartyPlaylists/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PartyPlaylists.Models;
+
+namespace PartyPlaylists.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
