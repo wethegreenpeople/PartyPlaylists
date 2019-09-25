fcd25b095d2de1040713ed5f4cc9fd14b2dcc0ad
John <wethegreenpeople@gmail.com>
2019-09-24 17:06:10 -0700

    Scaffold room objects Begin adding EF Core for data access

---
+++ PartyPlaylists/PartyPlaylists.MobileAppService/Contexts/PlaylistContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartyPlaylists.MobileAppService.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyPlaylists.MobileAppService.Contexts
+{
+    public class PlaylistContext : DbContext
+    {
+        public PlaylistContext(DbContextOptions<PlaylistContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
---
+++ PartyPlaylists/PartyPlaylists.MobileAppService/Models/RoomRepository.cs
@@ -0,0 +1,37 @@
+﻿using PartyPlaylists.MobileAppService.Interfaces;
+using PartyPlaylists.MobileAppService.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyPlaylists.MobileAppService.Models
+{
+    public class RoomRepository : IRoomRepository
+    {
+        public void Create(Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Room Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Room> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Room room)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
