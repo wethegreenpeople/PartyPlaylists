57109fcbeecb9ed3fc1ca39933aa268c61a80c84
John Singh <wethegreenpeople@gmail.com>
2019-09-24 22:40:26 -0700

    Add PlaylistContext migration

--- PartyPlaylists/PartyPlaylists.MobileAppService/Contexts/PlaylistContext.cs
+++ PartyPlaylists/PartyPlaylists.MobileAppService/Contexts/PlaylistContext.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
 using PartyPlaylists.MobileAppService.Models.DataModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +13,32 @@ namespace PartyPlaylists.MobileAppService.Contexts
 {
     public class PlaylistContext : DbContext
     {
+        public DbSet<Room> Rooms { get; set; }
+
         public PlaylistContext(DbContextOptions<PlaylistContext> options) : base(options)
         {
 
         }
 
-        public DbSet<Room> Rooms { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class PlaylistContextFactory : IDesignTimeDbContextFactory<PlaylistContext>
+    {
+        public PlaylistContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<PlaylistContext>();
+            var connectionString = configuration.GetConnectionString("PartyPlaylistConnectionString");
+            optionsBuilder.UseMySQL(connectionString);
+            return new PlaylistContext(optionsBuilder.Options);
+        }
     }
 }
