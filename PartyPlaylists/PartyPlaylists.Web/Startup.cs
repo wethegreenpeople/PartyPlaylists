90ac86ce7314bcb55603dbaf6352179ae13c57bc
John Singh <wethegreenpeople@gmail.com>
2019-10-02 17:38:04 -0700

    Add room route

--- PartyPlaylists/PartyPlaylists.Web/Startup.cs
+++ PartyPlaylists/PartyPlaylists.Web/Startup.cs
@@ -68,6 +68,10 @@ namespace PartyPlaylists.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "room",
+                    template: "room/{id}",
+                    defaults: new { controller = "Room", action = "Join" });
             });
         }
     }
