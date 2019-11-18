64e87711d6757c0043247b0fbdbd677d28f3562f
John Singh <wethegreenpeople@gmail.com>
2019-11-18 11:42:15 -0800

    Attempt to add JWT tracking to songs

---
+++ PartyPlaylists/PartyPlaylists/Models/DataModels/RoomToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartyPlaylists.Models.DataModels
+{
+    public class RoomToken
+    {
+        public int TokenId { get; set; }
+        public Token Token { get; set; }
+
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+
+        public List<Song> SongsVotedOn { get; set; }
+    }
+}
