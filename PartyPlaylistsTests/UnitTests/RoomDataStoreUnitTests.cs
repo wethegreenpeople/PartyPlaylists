2c68cd793c89ef2b9dcc849238c1ae3b6a7c9d52
John Singh <wethegreenpeople@gmail.com>
2020-01-04 15:49:19 -0800

    Move AddSongToRoom logic to roomdata store Add related unit tests

--- PartyPlaylistsTests/UnitTests/RoomDataStoreUnitTests.cs
+++ PartyPlaylistsTests/UnitTests/RoomDataStoreUnitTests.cs
@@ -14,6 +14,7 @@ namespace PartyPlaylistsTests.UnitTests
     public class RoomDataStoreUnitTests
     {
         private PlaylistContext _playlistContext;
+        private const string _token = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJpZCI6IjA1ZWVmMmM3LWJmNDctNDM3Zi05NjM0LWNiNjk2NjM3YWU2NCIsIm5hbWUiOiJTdG9ybSBIZWF0aGVyIn0.";
 
         [TestInitialize]
         public void TestInit()
@@ -47,6 +48,22 @@ namespace PartyPlaylistsTests.UnitTests
             var roomDataStore = new RoomDataStore(_playlistContext);
 
             await Assert.ThrowsExceptionAsync<ArgumentException>(async () => await roomDataStore.GetItemAsync("0"));
+        }
+
+        [TestMethod]
+        public async Task AddSongToRoomAsync_GivenRoomIDAndSong_ReturnRoomWithSong()
+        {
+            var roomdatastore = new RoomDataStore(_playlistContext);
+
+            var song = new Song() { Name = "Test Song", Id = 0, Artist = "Swaylo", SpotifyId = "1" };
+
+            Assert.AreEqual(0, (await roomdatastore.GetItemAsync("1")).RoomSongs.Count);
+
+            var room = await roomdatastore.AddSongToRoomAsync(_token, "1", song);
+
+            Assert.IsNotNull(room);
+            Assert.AreEqual(1, room.RoomSongs.Count);
+            Assert.AreEqual(1, (await roomdatastore.GetItemAsync("1")).RoomSongs.Count);
         }
     }
 }
