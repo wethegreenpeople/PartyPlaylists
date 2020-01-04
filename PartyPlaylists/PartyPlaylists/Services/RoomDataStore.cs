377a14799bce1e0bc56b6c5736526297aaf57943
John Singh <wethegreenpeople@gmail.com>
2020-01-04 15:49:19 -0800

    Move AddSongToRoom logic to roomdata store Add related unit tests

--- PartyPlaylists/PartyPlaylists/Services/RoomDataStore.cs
+++ PartyPlaylists/PartyPlaylists/Services/RoomDataStore.cs
@@ -5,6 +5,7 @@ using PartyPlaylists.MobileAppService.Contexts;
 using PartyPlaylists.Models.DataModels;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -84,22 +85,52 @@ namespace PartyPlaylists.Services
 
         public async Task<Room> AddSongToRoomAsync(string userToken, string roomId, Song song)
         {
-            if (roomId == null || song == null)
-                return null;
-
-            var patchMethod = new HttpMethod("PATCH");
-            var serializedItem = JsonConvert.SerializeObject(song);
-            var content = new StringContent(serializedItem, Encoding.UTF8, "application/json");
-
-            var request = new HttpRequestMessage(patchMethod, $@"{client.BaseAddress}room/{roomId}?userToken={userToken}")
+            try
             {
-                Content = content,
-            };
+                var roomIdAsInt = Convert.ToInt32(roomId);
+                var room = await GetItemAsync(roomId);
+                var playlist = _playlistsContext.SpotifyPlaylist.SingleOrDefaultAsync(s => s.RoomId == roomIdAsInt);
+                var matchingSong = _playlistsContext.Songs.FirstOrDefaultAsync(s => $"{s.Artist}{s.Name}" == $"{song.Artist}{song.Name}");
+                var decodedToken = JWT.Decode(userToken);
+                var token = JsonConvert.DeserializeAnonymousType(decodedToken, new { Name = "" });
 
-            var response = await client.SendAsync(request);
-            var respondedRoom = JsonConvert.DeserializeObject<Room>(await response.Content.ReadAsStringAsync());
+                if (room == null)
+                    throw new Exception("Could not find room");
 
-            return respondedRoom;
+                var roomSong = new RoomSong()
+                {
+                    RoomId = roomIdAsInt,
+                    SongId = (await matchingSong)?.Id ?? 0,
+                    Song = song,
+                    SongAddedBy = token.Name,
+                };
+
+                if (await playlist != null)
+                {
+                    var playlistTable = await playlist;
+                    var service = new SpotifyService(playlistTable.AuthCode);
+
+                    if (string.IsNullOrEmpty(song.SpotifyId))
+                    {
+                        var spotifySong = await service.GetSong(song.Name);
+                        await service.AddSongToPlaylist(playlistTable, spotifySong);
+                        song.SpotifyId = spotifySong.SpotifyId;
+                    }
+                    else
+                        await service.AddSongToPlaylist(playlistTable, song);
+                }
+                if (!room.RoomSongs.Any(s => s.SongId == roomSong.SongId))
+                {
+                    room.RoomSongs.Add(roomSong);
+                    await _playlistsContext.SaveChangesAsync();
+                }
+
+                return room;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
         public async Task<Room> AddSpotifyAuthCodeToRoomAsync(string roomId, string spotifyAuthCode)
