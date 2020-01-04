5a023a0514c9566901bc443e53a9969981b6c2b8
John Singh <wethegreenpeople@gmail.com>
2020-01-04 14:11:18 -0800

    Add test project

---
+++ PartyPlaylistsTests/UnitTests/RoomDataStoreTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartyPlaylistsTests.UnitTests
+{
+    [TestClass]
+    public class RoomDataStoreTests
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
