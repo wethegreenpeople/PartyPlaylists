b45208602ea47bf2728ce042122c4e51ed494de4
John Singh <wethegreenpeople@gmail.com>
2020-03-29 14:34:57 -0700

    Add join/create room cards

---
+++ PartyPlaylists.MVC/Models/ViewModels/IndexVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyPlaylists.MVC.Models.ViewModels
+{
+    public class IndexVM
+    {
+        [DisplayName("Room to Join")]
+        public string RoomToJoin { get; set; }
+
+        [DisplayName("Room Name")]
+        public string NewRoomName { get; set; }
+    }
+}
