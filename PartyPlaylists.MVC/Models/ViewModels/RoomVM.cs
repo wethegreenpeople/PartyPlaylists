b9931deb63a88e79a203b026c66dd54bf53a500d
John Singh <wethegreenpeople@gmail.com>
2020-03-29 20:52:58 -0700

    Create song list partial

---
+++ PartyPlaylists.MVC/Models/ViewModels/RoomVM.cs
@@ -0,0 +1,13 @@
+﻿using PartyPlaylists.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyPlaylists.MVC.Models.ViewModels
+{
+    public class RoomVM
+    {
+        public Room CurrentRoom { get; set; }
+    }
+}
