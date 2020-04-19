237b27651b4ae2fe5d55217d5bb6981ce41a294b
toastmalone <josh.m.gauntt@gmail.com>
2020-04-19 04:47:12 -0700

    Add up and down navigation of the search Move script code out of Index view and into a seperate file selectList.js Clean up repeated and inefficent code InProgress tests stop at sending the song to retrieve to the controller. Need to pull from master before that will work.

--- PartyPlaylists.MVC/wwwroot/js/selectList.js
+++ PartyPlaylists.MVC/wwwroot/js/selectList.js
@@ -1,92 +1,215 @@
-﻿var count = 1;
-var group = document.getElementById('slg');
-var list_group = group.querySelector('li ul');
-var list_array = group.querySelectorAll('li ul li');
-var search = document.getElementById('songToAdd');
+﻿//TODO add up and down arrow key selection.
+var itemPosition = 1;
+
+var songInputId = "songToAdd";
+var songListId = "songList";
+var hiddenInputId = "hiddenInput";
+var songListGroupId = "songListGroup";
+var songSearchContainerId = "searchContainer"
+
+var hiddenInput;
+var songListGroup;
+var songSearchContainer
+var songListItems;
+var songInput;
+
+var selectedListItem;
 
 $(document).ready(function () {
-    for (var i = 0; i < list_array.length; i++) {
-        list_array[i].dataset.display = 'true'
+    hiddenInput = document.getElementById(hiddenInputId);
+    songListGroup = document.getElementById(songListGroupId);
+    songList = document.getElementById(songListId);
+    songInput = document.getElementById(songInputId);
+    songSearchContainer = document.getElementById(songSearchContainerId);
+    var inputEventsCombined = function (e) {
+        searchFunc();
+        ShowSongList(songListGroup);
     }
+    songSearchContainer.addEventListener('mouseleave', function (event) {
+        songListGroup.setAttribute("data-toggle", "false");
+       
+    });
+
+    $('#songToAdd').on('input', inputEventsCombined);
+    songInput.addEventListener('keydown', function (e) { return KeyDownEventHandler(e) });
+    songInput.addEventListener("keydown" , function (e) {
+
+        if (e.keyCode == 13) {
+            var firstSearchItem = songListGroup.getElementsByTagName('li');
+            if (selectedListItem == null) {
+                SetSelectedListItem(firstSearchItem[0])
+            }
+            else {
+                postSongToAdd();
+            }
+           
+        }
+    })
+
+    var inputClickEventFunctions = function () {
+        if (songInput.value === "" || songInput.value === null) {
+            return;
+        }
+        ShowSongList(songListGroup);
+    }
+
+
+    songInput.addEventListener('click', inputClickEventFunctions);
 })
-search.addEventListener('input', function (e) {
-    show_list(list_group);
-    key_up_down();
-});
 
-search.addEventListener('click', function (e) {
-    init_list();
-    show_list(list_group);
-    key_up_down();
-});
 
-function init_list() {
-    count = 0;
-    for (var i = 0; i < list_array.length; i++) {
-        init_item(list_array[i]);
-        list_array[i].addEventListener('click', copy_paste);
-    }
+
+function InitializeListItems() {
+    itemPosition = songListItems.length;
+    Array.prototype.forEach.call(songListItems, s => {
+        s.addEventListener('click', function () {
+            SetSelectedListItem(s);
+            postSongToAdd();
+            
+        });
+        s.addEventListener("mouseenter", function (event) {
+            SetSelectedListItem(s);
+        });
+    });
 }
 
-function init_item(item) {
+function InitializeItem(item) {
     item.dataset.display = 'true';
     item.dataset.highlight = 'false';
 }
 
-function copy_paste() {
-    search.value = this.innerHTML;
-    // todo : check match of list text and input value for .current 
-    init_list();
-    hide_list(list_group);
-}
-
-function hide_list(ele) {
-    ele.dataset.toggle = 'false'
-}
-
-function show_list(ele) {
-    ele.dataset.toggle = 'true'
-}
-
-function key_up_down() {
-
-    var items = group.querySelectorAll('li[data-display="true"]');
-    var last = items[items.length - 1];
-    var first = items[0];
-
-    search.onkeydown = function (e) {
-
-        if (e.keyCode === 38) {
-            count--;
-            count = count <= 0 ? items.length : count;
-            items[count - 1].dataset.highlight = items[count - 1] ? 'true' : 'false';
-            if (items[count]) {
-                items[count].dataset.highlight = 'false';
-            }
-            else {
-                first.dataset.highlight = 'false';
-            }
-        }
-
-        if (e.keyCode === 40) {
-            items[count].dataset.highlight = items[count] ? 'true' : 'false';
-            if (items[count - 1]) {
-                items[count - 1].dataset.highlight = 'false';
-            }
-            else {
-                last.dataset.highlight = 'false';
-            }
-            count++;
-            count = count >= items.length ? 0 : count;
-        }
-    };
-}
-
-group.addEventListener('mouseleave', function (event) {
-    if (event.target != list_group && event.target.parentNode != list_group) {
-        list_group.dataset.toggle = 'false'
+function HideSongListItem(ele) {
+    if (ele == null) {
+        return;
     }
-});
+    songListGroup.setAttribute("data-toggle", "false");
+}
+
+function ShowSongList(ele) {
+    if (ele == null) {
+        return;
+    }
+    songListGroup.setAttribute("data-toggle", "true");
+}
+
+function SetSelectedListItem(item) {
+    if (selectedListItem == null) {
+        selectedListItem = item;
+        selectedListItem.dataset.highlight = 'true';
+    }
+    else {
+        selectedListItem.dataset.highlight = 'false';
+        selectedListItem = item;
+        selectedListItem.dataset.highlight = 'true';
+    }
+
+}
+
+function KeyDownEventHandler(e) {
+
+    var items = songListGroup.getElementsByTagName("li");
+
+    //backspace
+    if (e.keyCode === 8) {
+        if (songInput.value.length <= 1 || songInput.value === null) {
+                HideSongListItem(songListGroup);
+        }
+    }
+
+    //up
+    if (e.keyCode === 38) {
+        itemPosition--;
+
+        if (itemPosition > items.length || itemPosition < 0) {
+            itemPosition = 0;
+
+        }
+
+        SetSelectedListItem(items[itemPosition]);
+    }
+    //down
+    else if (e.keyCode === 40) {
+        itemPosition++;
+
+        if (itemPosition > items.length || itemPosition < 0) {
+            itemPosition = 0;
+            
+        }
+
+        SetSelectedListItem(items[itemPosition]);
+    }
+
+
+
+    
+
+}
+
+var searchFunc = function () {
+    var auth = $('#spotifyAuthCode').val();
+    $.ajax({
+        url: "/room/livesearch",
+        type: "GET",
+        async: true,
+        data: {
+            "auth": auth,
+            "query": $('#songToAdd').val()
+        },
+        success: function (response) {
+
+            if (response == null) {
+                return;
+            }
+
+            songListGroup.innerHTML = response;
+            songListItems = songListGroup.childNodes;
+            InitializeListItems();
+        },
+
+        error: function (response) {
+            console.log("Failed to get search results. " + response);
+        }
+    })
+}
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(null, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(null, args);
+    };
+};
+
+function postSongToAdd() {
+    if (selectedListItem != null) {
+        hiddenInput.value = selectedListItem.id;
+        $.ajax({
+            url: "/room/AddSongToRoom",
+            type: "POST",
+            data: {
+                "CurrentRoom.Id": $("#roomId")[0].value,
+                "CurrentRoom.SpotifyAuthCode": $("#spotifyAuthCode")[0].value,
+                "SongToAdd": hiddenInput.value,
+            },
+            headers: {
+                'Authorization': "Bearer " + $("#jwtToken")[0].value,
+            },
+            success: function (data) {
+                $("#tableOfSongs").html(data);
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        });
+    }
+}
 
 
 
