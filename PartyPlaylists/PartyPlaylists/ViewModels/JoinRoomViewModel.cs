86504cf2438ea9cb08e003ef3ebc7b96f1e76461
John Singh <wethegreenpeople@gmail.com>
2020-04-19 14:19:09 -0700

    Add join room view and viewmodel

---
+++ PartyPlaylists/PartyPlaylists/ViewModels/JoinRoomViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PartyPlaylists.ViewModels
+{
+    public class JoinRoomViewModel : BaseViewModel
+    {
+        public JoinRoomViewModel()
+        {
+            Title = "Join a Room";
+        }
+    }
+}
