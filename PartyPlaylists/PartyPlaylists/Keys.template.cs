8d24cd3eaa664a0d35bec76e23361762fe0606a5
John Singh <wethegreenpeople@gmail.com>
2020-04-23 18:29:58 -0700

    Clarify how the keys file works a little

--- PartyPlaylists/PartyPlaylists/Keys.template.cs
+++ PartyPlaylists/PartyPlaylists/Keys.template.cs
@@ -7,6 +7,9 @@ using System.Text;
 // of the repo. It has duplicated information from our standard appsettings.json file
 // which already gets duplicated far too many times.
 // We need a better way of implementing both this file, and the appsettings.json in general.
+
+// Remove .template from the namespace, and file name
+// Fill in whatever key portions you need, and you should be good to go.
 namespace PartyPlaylists.Droid.Template
 {
     public static class Keys
