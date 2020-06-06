0e1a945aa69c605b7e9ed8e4564a2d0dfe1cc47c
wethegreenpeople <superjohn007@gmail.com>
2020-06-06 09:19:48 -0700

    * Main.cs: * Info.plist: * FileStorage.cs:
    * PartyPlaylists.iOS.csproj: Add required dependancies to iOS project

---
+++ PartyPlaylists/PartyPlaylists.iOS/FileStorage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PartyPlaylists.Services;
+
+namespace PartyPlaylists.iOS
+{
+    public class FileStorage : IFileStorage
+    {
+        public Task<byte[]> ReadAsBytes(string filename)
+        {
+            var data = File.ReadAllBytes(filename);
+
+            if (data != null)
+                data = CleanByteOrderMark(data);
+
+            return Task.FromResult(data);
+        }
+
+        public async Task<string> ReadAsString(string filename)
+        {
+            var data = await ReadAsBytes(filename);
+
+            if (data == null)
+                return string.Empty;
+
+            return System.Text.Encoding.UTF8.GetString(data);
+        }
+
+        public byte[] CleanByteOrderMark(byte[] bytes)
+        {
+            var bom = new byte[] { 0xEF, 0xBB, 0xBF };
+            var empty = Enumerable.Empty<byte>();
+            if (bytes.Take(3).SequenceEqual(bom))
+                return bytes.Skip(3).ToArray();
+
+            return bytes;
+        }
+    }
+}
--- PartyPlaylists/PartyPlaylists.iOS/Main.cs
+++ PartyPlaylists/PartyPlaylists.iOS/Main.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using Microsoft.AspNetCore.SignalR.Client;
+using Splat;
 using UIKit;
 
+using PartyPlaylists.Services;
 namespace PartyPlaylists.iOS
 {
     public class Application
@@ -12,6 +15,11 @@ namespace PartyPlaylists.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            Locator.CurrentMutable.RegisterLazySingleton(() => new HubConnectionBuilder().WithUrl($"https://partyplaylists.azurewebsites.net/roomhub").Build(), typeof(HubConnection));
+            Locator.CurrentMutable.RegisterLazySingleton(() => new SpotifyMobileSDK(), typeof(ISpotifyMobileSDK));
+            Locator.CurrentMutable.RegisterLazySingleton(() => new CustomConfig(), typeof(ICustomConfig));
+            Locator.CurrentMutable.RegisterLazySingleton(() => new FileStorage(), typeof(IFileStorage));
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
